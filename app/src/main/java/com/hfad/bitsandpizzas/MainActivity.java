66857129dbf8cad2d30492ccfa7eb25c144a1824
Bada Adedayo <deetu02@gmail.com>
2016-12-12 13:51:52 +0000

    Changed the toolbar color

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends Activity {
                 }
         );
 
-        Drawable d=getResources().getDrawable(R.drawable.brickred);
+        Drawable d = getResources().getDrawable(R.drawable.brickred);
         getActionBar().setBackgroundDrawable(d);
 
     }
